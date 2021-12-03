553ae166043f417c9de2412ef5068662b9b8b016
Mihai F <Miha>
2021-12-03 19:23:31 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbersList = new ArrayList<>();
+        for (int i = 1; 30 >= i; i++) {
+            numbersList.add(i);
+        }
+        System.out.print("The numbers stored in the list are: ");
+        for (int i = 0; i < numbersList.size(); i++) {
+            System.out.print(numbersList.get(i) + " ");
+        }
+        Random random = new Random();
+        randomInt = numbersList;
+
+    }
+}
